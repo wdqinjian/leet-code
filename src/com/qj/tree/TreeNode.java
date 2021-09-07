1396bce1f7bffdb6b20a7c832e73487fb56972a3
秦健 <17621684830@163.com>
2021-09-07 22:58:18 +0800

    二叉树的后序遍历_145

--- src/com/qj/tree/TreeNode.java
+++ src/com/qj/tree/TreeNode.java
@@ -21,4 +21,20 @@ public class TreeNode {
         this.left = left;
         this.right = right;
     }
+
+    public static TreeNode buildNode() {
+
+        TreeNode right3 = new TreeNode(14);
+        TreeNode left3 = new TreeNode(6);
+
+        TreeNode right2 = new TreeNode(13);
+        TreeNode left2 = new TreeNode(7);
+
+        TreeNode right1 = new TreeNode(11, left3, right3);
+        TreeNode left1 = new TreeNode(8, left2, right2);
+
+        TreeNode root = new TreeNode(10, left1, right1);
+
+        return root;
+    }
 }
--- src/com/qj/tree/二叉树的后序遍历_145.java
+++ src/com/qj/tree/二叉树的后序遍历_145.java
@@ -1,14 +1,46 @@
 package com.qj.tree;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * @author qinjian
  */
 public class 二叉树的后序遍历_145 {
 
+    public static void main(String[] args) {
+        二叉树的后序遍历_145 demo = new 二叉树的后序遍历_145();
+        TreeNode treeNode = TreeNode.buildNode();
+        List<Integer> integers = demo.postorderTraversal(treeNode);
+        integers.forEach(System.out::println);
+
+    }
+
     public List<Integer> postorderTraversal(TreeNode root) {
+        LinkedList<Integer> result = new LinkedList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.isEmpty()) {
+            if (root != null) {
+                stack.push(root);
+                result.addFirst(root.val);
+                root = root.right;
+            } else {
+                root = stack.pop();
+                root = root.left;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 递归解法
+     *
+     * @param root
+     * @return
+     */
+    public List<Integer> postorderTraversal1(TreeNode root) {
         List<Integer> list = new ArrayList<>();
         postorder(root, list);
         return list;
