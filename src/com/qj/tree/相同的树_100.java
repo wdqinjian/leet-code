2eb8d1ad16cac286249ec59bb952cab12f73eff8
秦健 <17621684830@163.com>
2021-09-11 22:16:55 +0800

    零钱兑换_322

---
+++ src/com/qj/tree/相同的树_100.java
@@ -0,0 +1,20 @@
+package com.qj.tree;
+
+/**
+ * @author qinjian
+ */
+public class 相同的树_100 {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q == null) {
+            return false;
+        } else if (p.val != q.val) {
+            return false;
+        } else {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+    }
+
+}
