94f8360f3d230842dbcfbac8297842436683d28f
秦健 <17621684830@163.com>
2021-11-02 22:16:21 +0800

    提交

--- src/com/qj/array/和为K的子数组_560.java
+++ src/com/qj/array/和为K的子数组_560.java
@@ -1,10 +1,44 @@
 package com.qj.array;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author qinjian
  */
 public class 和为K的子数组_560 {
+
+    public static void main(String[] args) {
+        和为K的子数组_560 demo = new 和为K的子数组_560();
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int i = demo.subarraySum(nums, 7);
+        System.out.println(i);
+    }
+
     public int subarraySum(int[] nums, int k) {
+        // key：前缀和，value：key 对应的前缀和的个数
+        Map<Integer, Integer> preSumFreq = new HashMap<>(nums.length + 1, 1);
+        // 对于下标为 0 的元素，前缀和为 0，个数为 1
+        preSumFreq.put(0, 1);
+
+        int preSum = 0;
+        int count = 0;
+        for (int num : nums) {
+            preSum += num;
+
+            // 先获得前缀和为 preSum - k 的个数，加到计数变量里
+            if (preSumFreq.containsKey(preSum - k)) {
+                count += preSumFreq.get(preSum - k);
+            }
+
+            // 然后维护 preSumFreq 的定义
+            preSumFreq.put(preSum, preSumFreq.getOrDefault(preSum, 0) + 1);
+        }
+        return count;
+    }
+
+
+    public int subarraySum1(int[] nums, int k) {
 
         int n = nums.length;
         int[] temp = new int[n + 1];
---
+++ src/com/qj/linkedList/环形链表_141.java
@@ -0,0 +1,34 @@
+package com.qj.linkedList;
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) {
+ * val = x;
+ * next = null;
+ * }
+ * }
+ *
+ * @author qinjian
+ */
+public class 环形链表_141 {
+
+
+    public boolean hasCycle(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
